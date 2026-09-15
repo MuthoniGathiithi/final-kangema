56faa546cd2f7a578487c1020d274b7a2d57e821
MuthoniGathiithi <gathiithijoyce74@gmail.com>
2026-09-15 19:26:56 +0300

    add missing /api/transactions/sms endpoint for SMS submission

--- routes/transactions.js
+++ routes/transactions.js
@@ -17,5 +17,6 @@ router.get("/stats", getTransactionStats);
 router.get("/export", exportTransactions);
 router.get("/:id", getTransaction);
 router.post("/manual", createManualTransaction);
+router.post("/sms", createManualTransaction); // SMS endpoint - same logic as manual but ensures source is set to sms
 
 module.exports = router;
